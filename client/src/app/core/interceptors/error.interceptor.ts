1103360d34606c02dcf52c2f666bf8b48ffaa6a7
Wongkot <wongkot.v@gmail.com>
2020-09-07 00:06:27 +0700

    End of section 11

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,43 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(
+        private router: Router,
+        private toastr: ToastrService,
+    ) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(req).pipe(
+            catchError((errorRes: HttpErrorResponse) => {
+                if (errorRes) {
+                    if (errorRes.status === 400) {
+                        if (errorRes.error.errors) {
+                            throw errorRes.error;
+                        } else {
+                            this.toastr.error(errorRes.error.message, errorRes.error.statusCode);
+                        }
+                    }
+                    if (errorRes.status === 401) {
+                        this.toastr.error(errorRes.error.message, errorRes.error.statusCode);
+                    }
+                    if (errorRes.status === 404) {
+                        this.router.navigateByUrl('/not-found');
+                    }
+                    if (errorRes.status === 500) {
+                        const navigationExtras: NavigationExtras = {
+                            state: { error: errorRes.error }
+                        };
+                        this.router.navigateByUrl('/server-error', navigationExtras);
+                    }
+                }
+                return throwError(errorRes);
+            })
+        );
+    }
+}
---
+++ client/src/app/core/server-error/server-error.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  templateUrl: './server-error.component.html',
+  styleUrls: ['./server-error.component.scss']
+})
+export class ServerErrorComponent implements OnInit {
+  error: any;
+
+  constructor(
+    private router: Router,
+  ) {
+    // NOTE: NavigationExtras only available in constructor
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation
+      && navigation.extras
+      && navigation.extras.state
+      && navigation.extras.state.error;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
